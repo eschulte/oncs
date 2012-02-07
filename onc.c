9f9c43afcb6d9e815192b4710fcfe15c89f6d738
Eric Schulte <eric.schulte@gmx.com>
2012-02-06 20:58:48 -0700

    recorded integer values of headers

--- onc.c
+++ onc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /* see https://en.wikipedia.org/wiki/Bitfield */
-typedef int header;
+typedef unsigned char header;
 #define first  (1 << 0)          /* 0x01 */
 #define second (1 << 1)          /* 0x01 */
 #define third  (1 << 2)          /* 0x02 */
@@ -13,14 +13,49 @@ void header_reset(header *self, int flag){ *self &= ~flag; }
 void   header_rev(header *self, int flag){ *self ^= flag;  }
 int    header_get(header *self, int flag){ return (*self & flag) != 0; }
 
-int main(){
-  header head = header_new();
-  header_set(&head, second);
-  printf("%d %d %d %d\n",
+void display(header head){
+  printf("%d %d %d %d %d\n", head,
          header_get(&head, first),
          header_get(&head, second),
          header_get(&head, third),
          header_get(&head, fourth));
+}
+
+void process_header(header head){
+  switch( head ){
+  case 7:                       /* 1 1 1 0 */
+    /* look up variable in env and direct to that location */
+    break; 
+  case 3:                       /* 1 1 0 0 */
+    /* send message in local direction */
+    break; 
+  case 5:                       /* 1 0 1 0 */
+    /* look up value in car and direct there */
+    break; 
+  case 1:                       /* 1 0 0 0 */
+    /* look up value in cdr and direct there */ 
+   break; 
+  case 6:                       /* 0 1 1 0 */
+    /* store subsequent payload in message */
+    break; 
+  case 2:                       /* 0 1 0 0 */
+    /* store subsequent payload in env */
+    break; 
+  case 4:                       /* 0 0 1 0 */
+    /* store subsequent payload in car */
+    break; 
+  case 0:                       /* 0 0 0 0 */
+    /* store subsequent payload in cdr */
+    break; 
+  }
+}
+
+int main(){
+  header head = header_new();
+  header_set(&head, second);
+  display(head);
+  process_header(head);
+  return 0;
 }
 
 /* /\* Message *\/ */
