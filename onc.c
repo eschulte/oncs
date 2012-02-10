242093aa34781885f03d6d1c56e370dd4ab74ad8
Eric Schulte <eric.schulte@gmx.com>
2012-02-10 07:10:48 -0700

    global queue just for now?

--- onc.c
+++ onc.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define SIZE 1000
+#define QUEBITS 64064 /* TODO: assoc onc with every payload & header?  */
 
 /* ONC */
 struct onc        // +----------------+
@@ -16,7 +18,10 @@ typedef unsigned char header;
 /* Message Payloads */
 typedef unsigned long long word;
 
-void process_header(struct onc self, header head){
+/* Message Queue -- TODO: this shouldn't be global, just getting started */
+uchar mque[QUEBITS];
+
+void accept_message_header(struct onc self, header head){
   switch( head ){
   case 7: /* 1 1 1 0 -- direct to value of payload in env */    break;
   case 3: /* 1 1 0 0 -- direct to local direction of payload */ break;
@@ -29,7 +34,30 @@ void process_header(struct onc self, header head){
   }
 }
 
-int main(){
-  printf("ONCs are not Conses\n");
+void accept_message_payload(struct onc self, word payload){
+  switch( head ){
+  case 7: /* 1 1 1 0 -- direct to value of payload in env */    break;
+  case 3: /* 1 1 0 0 -- direct to local direction of payload */ break;
+  case 5: /* 1 0 1 0 -- direct to value of car */               break;
+  case 1: /* 1 0 0 0 -- direct to value of cdr */               break;
+  case 6: /* 0 1 1 0 */ onc.msg = payload;                      break;
+  case 2: /* 0 1 0 0 */ onc.env = payload;                      break;
+  case 4: /* 0 0 1 0 */ onc.car = payload;                      break;
+  case 0: /* 0 0 0 0 */ onc.cdr = payload;                      break;
+  }
+}
+
+struct onc medium[SIZE][SIZE];
+
+int main(int argc, const char* argv[]){
+  int steps = atoi(argv[1]);
+  int step, x, y;
+  for(step=0; step<=steps; step++){
+    for(x=0; x<SIZE; x++){
+      for(y=0; y<SIZE; y++){
+        
+      }
+    }
+  }
   return 0;
 }
