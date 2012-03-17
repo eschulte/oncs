c94bd456bb7cfc11fa1d0a15154d576e896c4320
Eric Schulte <eric.schulte@gmx.com>
2012-03-17 18:40:43 -0400

    more thorough check of duplication reveals errors

--- test/duplicate.c
+++ test/duplicate.c
@@ -22,6 +22,20 @@ int main(int argc, char *argv[]){
   SHOULD(population() == 4);
   if(verbose_p) show_world();
 
+  /* duplicate four more times */
+  int k,l,counter;
+  counter = 0;
+  for(l=1;l<=4;l++){
+    for(k=1;k<=4;k++){
+      counter++;
+      new.x = 4+k % SIZE;
+      new.y = 4+l % SIZE;
+      duplicate(new, place);
+      if(verbose_p) show_world();
+      SHOULD(population() == (4 + (2 * counter)));
+    }
+  }
+
   /* return indicates success or failure */
   return fail_p;
 }
