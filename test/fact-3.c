b8e3b2fcbcb0bd1587873a3e548a725d39b85b12
Eric Schulte <eric.schulte@gmx.com>
2012-04-25 12:27:34 -0400

    probably prim-opts need similar pre-exec checks to lambdas

--- test/fact-3.c
+++ test/fact-3.c
@@ -4,14 +4,10 @@ int main(int argc, char *argv[]){
   init(argc, argv);
 
   char expr[] =
-    "(#L1 ((= #S1 0)"
-    "       1"
-    "       (* #S1"
-    "          ((#L0 (= #S0 0 1 (* #S0 1)))"
-    "           (- 1 #S1)))))"
-    "1";
+    "(#L1 (= #S1 0 1 (* #S1 ((#L0 (= #S0 0 1 (* #S0 (- 1 #S0)))) - 1 #S1))))"
+    "2";
   expr_to_expr(expr);
-  SHOULD(strcmp(expr, "1") == 0);
+  SHOULD(strcmp(expr, "2") == 0);
 
   /* return indicates success or failure */
   return fail_p;
